4733588af6e5c49bff625d1facd49fdc50b2c3f9
Sandipa Paul <sandipapaul2004@gmail.com>
2024-04-20 12:50:42 +0530

    first commit

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,6 @@
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
